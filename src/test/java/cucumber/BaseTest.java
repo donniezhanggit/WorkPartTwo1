327852f62a0f87bbbd63c34feabf725227a65dd0
levgren <jekaobodianskyi@gmail.com>
2019-05-04 19:08:18 +0300

    success, need to check loging

--- src/test/java/cucumber/BaseTest.java
+++ src/test/java/cucumber/BaseTest.java
@@ -12,10 +12,9 @@ import static com.codeborne.selenide.Selenide.page;
 
 public class BaseTest {
 
-//    public MainPage mainPage = new MainPage();
-    public MainPage mainPage = page(MainPage.class);
-    public SearchResultPage searchResultPage = page(SearchResultPage.class);
-    public WebSitePage webSitePage = page(WebSitePage.class);
+    MainPage mainPage = page(MainPage.class);
+    SearchResultPage searchResultPage = page(SearchResultPage.class);
+    WebSitePage webSitePage = page(WebSitePage.class);
 
     private static FileInputStream fileInputStream;
     private static Properties properties;
@@ -24,7 +23,7 @@ public class BaseTest {
     protected static  String domain;
     protected static  String searchedWord;
 
-@BeforeClass
+    @BeforeClass
     public static void setProperties() {
         try {
             fileInputStream = new FileInputStream("config.properties");
@@ -32,13 +31,6 @@ public class BaseTest {
             properties.load(fileInputStream);
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            if (fileInputStream != null)
-                try {
-                    fileInputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
         }
 
         url = properties.getProperty("url");
